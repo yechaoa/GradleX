bbf4ea6ad0d3f569fabda38ff26fc9978b2b91f4
yechao <best_yc@163.com>
2023-09-04 01:14:07 +0800

    add log

--- app/src/main/java/com/yechaoa/gradlex/MainActivity.kt
+++ app/src/main/java/com/yechaoa/gradlex/MainActivity.kt
@@ -2,10 +2,23 @@ package com.yechaoa.gradlex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.v("yechaoa","onResume")
+        Log.d("yechaoa","onResume")
+        Log.i("yechaoa","onResume")
+        Log.w("yechaoa","onResume")
+        Log.e("yechaoa","onResume")
+        Log.wtf("yechaoa wtf","onResume by wtf")
+    }
+
 }
