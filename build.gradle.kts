b00bd709d8780e37f5351ea618343e9cba24aed9
yechao <best_yc@163.com>
2024-08-29 01:30:29 +0800

    add Setup Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
 //    id("com.android.application") version "8.1.1" apply false
-    id("com.android.library") version "8.1.1" apply false
+    id("com.android.library") version "8.5.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.dorongold.task-tree") version "2.1.1"
 }
